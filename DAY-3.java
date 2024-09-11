65d4ac00449ba0bcb87d6763aadc29384b8d00d9
Abish <105860062+Abish-DM@users.noreply.github.com>
2024-09-11 23:46:59 +0530

    DAY-3.java

---
+++ DAY-3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Day3
+{
+     public static void main(String abi[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in array");
+        int n=sc.nextInt(),ar[]=new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            ar[i]=sc.nextInt();
+        }
+        System.out.print("Duplicate number is:");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(ar[i]==ar[j])
+                {
+                  System.out.println(ar[i]);
+                }
+                break;
+            }
+        }
+    }
+}
